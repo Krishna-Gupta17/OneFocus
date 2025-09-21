853fb93cf9dcff50b96614e74b01100d09214152
Krishna-Gupta17 <2024021228@mmmut.ac.in>
2025-09-22 02:01:57 +0530

    ||

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import App from './App.jsx';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'boxicons/css/boxicons.min.css';
@@ -8,7 +8,7 @@ import './index.css'; // Tailwind (via PostCSS build, not CDN)
 
 
 createRoot(document.getElementById('root')!).render(
-  < BrowserRouter>
+  < HashRouter>
     <App />
-  </ BrowserRouter>
+  </ HashRouter>
 );
